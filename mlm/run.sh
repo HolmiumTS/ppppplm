c941348d0f523f8c1c58ba2649ffb9c9736ef61e
HolmiumTS <42644820+holmiumts@users.noreply.github.com>
2021-05-21 12:11:11 +0800

    upd

--- mlm/run.sh
+++ mlm/run.sh
@@ -8,9 +8,9 @@ python ./mlm.py \
              --train_file ../../bertweet_data/train_data.txt \
              --max_seq_length 512 \
              --num_train_epochs 20 \
-             --per_device_train_batch_size 6 \
-             --per_device_eval_batch_size 6 \
-             --gradient_accumulation_steps 42 \
+             --per_device_train_batch_size 4 \
+             --per_device_eval_batch_size 4 \
+             --gradient_accumulation_steps 63 \
              --save_steps 500 \
              --cache_dir ../../.cache \
              --fp16 \
