df47a43c5c93593614a158c03134740fdf47fcc3
HolmiumTS <42644820+HolmiumTS@users.noreply.github.com>
2021-05-25 19:49:30 +0800

    更新了pretrain的超参数

--- mlm/run.sh
+++ mlm/run.sh
@@ -2,7 +2,7 @@
 
 python ./mlm.py \
              --model_name_or_path roberta-base \
-             --output_dir ../../bertweet-v1.1/ \
+             --output_dir ../../bertweet-v1.2/ \
              --seed 42 \
              --do_train \
              --train_file ../../bertweet_data/train_data.txt \
@@ -10,8 +10,16 @@ python ./mlm.py \
              --num_train_epochs 20 \
              --per_device_train_batch_size 4 \
              --per_device_eval_batch_size 4 \
-             --gradient_accumulation_steps 63 \
-             --save_steps 500 \
+             --gradient_accumulation_steps 64 \
+             --save_steps 100 \
              --cache_dir ../../.cache \
              --fp16 \
+             --learning_rate 7e-4 \
+             --weight_decay 0.01 \
+             --adam_beta1 0.9 \
+             --adam_beta2 0.98 \
+             --adam_epsilon 1e-6 \
+             --lr_scheduler_type linear \
+             --warmup_steps 2000 \
+             --logging_steps 100 \
              --dataloader_num_workers 4
